90525350da3631e5816110baab8690b175c488dd
Mehedi Hasan <you@example.com>
2023-04-06 12:42:28 +0600

    feat: socket configeration done

--- server.js
+++ server.js
@@ -14,6 +14,22 @@ io.on('connection', (socket) => {
   console.log('a user connected');
   socket.on('disconnect', () => {
     console.log('user disconnected');
+    io.emit('player-left', socket.id);
+  });
+  socket.on('player-join', () => {
+    const player = { id: socket.id, symbol: players.length === 0 ? 'X' : 'O' };
+    players.push(player);
+    console.log('player joined', player);
+    socket.emit('player-joined', player);
+    io.emit('player-join', player);
+  });
+  socket.on('player-move', (data) => {
+    console.log('player move', data);
+    socket.broadcast.emit('player-move', data);
+  });
+  socket.on('game-end', (data) => {
+    console.log('game end', data);
+    io.emit('game-end', data);
   });
 });
 
@@ -24,7 +40,7 @@ server.listen(PORT, () => {
 
 // ----
 
-io.on('connection', (socket) => {
+/* io.on('connection', (socket) => {
   console.log('a user connected');
   socket.on('disconnect', () => {
     console.log('user disconnected');
@@ -45,4 +61,4 @@ io.on('connection', (socket) => {
     console.log('game end', data);
     io.emit('game-end', data);
   });
-});
+}); */
